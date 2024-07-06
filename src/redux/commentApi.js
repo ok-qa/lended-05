1abffd24d8b530105ff002a5e9f4d98923868320
ok-qa <olechka.kr@gmail.com>
2024-07-06 05:10:11 -0700

    api added
    Updated files Home.jsx, Grid.jsx and Comment.jsx

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://667393ca6ca902ae11b4a00d.mockapi.io";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => API_ENDPOINT,
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery } = commentApi;
